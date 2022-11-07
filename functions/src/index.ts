c681af78b5a29c087cb771b153e4e4a83923d3dd
diskszk <ibalucky23@gmail.com>
2022-11-07 20:23:59 +0900

    feat: add cors options

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,15 +2,19 @@ import { https } from "firebase-functions";
 import * as admin from "firebase-admin";
 import { ALBUMS } from "./constants";
 import { fetchAlbums } from "./query";
+import * as cors from "cors";
+
+cors;
 
 admin.initializeApp();
 
 export const albumsRef = admin.firestore().collection(ALBUMS);
-
 export const albums = https.onRequest(async (req, res) => {
-  if (req.method !== "GET") {
-    res.status(405);
-  }
+  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Methods", ["GET"]);
+  // if (req.method !== "GET") {
+  //   res.status(405);
+  // }
 
   try {
     const albums = await fetchAlbums();
