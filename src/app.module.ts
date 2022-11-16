e518dbb3f63f0291278541d2b37ef487ec25f32f
diskszk <ibalucky23@gmail.com>
2022-11-16 13:42:57 +0900

    chore: configを修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { AlbumsModule } from "./albums/albums.module";
 
 @Module({
   imports: [AlbumsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
