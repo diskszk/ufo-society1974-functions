7f7475b42702f8ad9038f1e4c9e19c971626fef8
diskszk <49810133+diskszk@users.noreply.github.com>
2022-11-16 13:43:24 +0900

    Feat/add get albums (#7)
    * feat: setup test
    
    * feat: getAlbums関数のテストを追加する
    
    * feat: firestoreと接続する
    
    * feat: リファクタリング
    
    * chore: configを修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AlbumsModule } from "./albums/albums.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AlbumsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import * as express from "express";
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
 
 import helmet from "helmet";
 import * as cors from "cors";
 
-import { PUBLISHED_ALBUMS, SONGS } from "./constants";
 import { AppModule } from "./app.module";
-
-admin.initializeApp();
-
-const db = admin.firestore();
-export const refs = {
-  albumsRef: db.collection(PUBLISHED_ALBUMS),
-  songsRef: (albumId: string) =>
-    db.collection(PUBLISHED_ALBUMS).doc(albumId).collection(SONGS),
-};
 
 const server = express();
 
