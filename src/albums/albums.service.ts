b6f581d5ab6c52b8e9abe49a4ec238b5cbc8b06c
diskszk <ibalucky23@gmail.com>
2022-11-16 12:09:27 +0900

    feat: firestoreと接続する

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { albumConverter } from "../converter";
 import { Album } from "ufo-society1974-definition-types";
-import { mockAlbums } from "../mock/albums";
+
+import { PUBLISHED_ALBUMS, PUBLISHED_DATE } from "../constants";
+import { connectFirestore } from "../connectFirestore";
+
+const { db } = connectFirestore();
 
 @Injectable()
 export class AlbumsService {
   async getAlbums(): Promise<Album[]> {
-    return [...mockAlbums];
+    const snapshots = await db
+      .collection(PUBLISHED_ALBUMS)
+      .orderBy(PUBLISHED_DATE, "desc")
+      .withConverter(albumConverter)
+      .get();
+
+    const albums: Album[] = snapshots.docs.map((snapshot) => {
+      const doc = snapshot.data();
+
+      return { ...doc, id: snapshot.id };
+    });
+    return albums;
   }
 }
