0b6253c225bb6dbe338e6a183efdc281dde690a3
diskszk <49810133+diskszk@users.noreply.github.com>
2022-11-16 16:50:54 +0900

    feat: add getAlbumById (#8)

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from "@nestjs/common";
 import { Album } from "ufo-society1974-definition-types";
 import { AlbumsService } from "./albums.service";
 
@@ -9,5 +15,21 @@ export class AlbumsController {
   @Get()
   fetchAlbums(): Promise<Album[]> {
     return this.albumsService.findAll();
+  }
+
+  @Get(":albumId")
+  async findAlbumById(@Param("albumId") albumId: string): Promise<Album> {
+    const album = await this.albumsService.findById(albumId);
+    if (!album) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: "Missing album id.",
+        },
+        404
+      );
+    }
+
+    return album;
   }
 }
