01e5d1482050cf7263cdcb262936ed467cca8df6
diskszk <ibalucky23@gmail.com>
2022-12-05 23:08:17 +0900

    feat: roleをチエックするguardを作成する

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from "./users/users.module";
 import { AlbumsModule } from "./albums/albums.module";
 import { AuthModule } from "./auth/auth.module";
 import { AppController } from "./app.controller";
+import { RoleModule } from "./role/role.module";
 
 @Module({
-  imports: [AlbumsModule, AuthModule, UsersModule],
+  imports: [AlbumsModule, AuthModule, UsersModule, RoleModule],
   controllers: [AppController],
   providers: [],
 })
---
+++ src/role/role.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { RoleType } from "../types";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  private validateRole(
+    validRole: RoleType,
+    userRole: RoleType | undefined
+  ): boolean {
+    if (!userRole) {
+      return false;
+    }
+
+    return validRole === userRole;
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const validRole = this.reflector.get<RoleType>(
+      "role",
+      context.getHandler()
+    );
+
+    const requests = context.switchToHttp().getRequest<Request>();
+
+    const userRole = requests.headers["role"] as RoleType | undefined;
+
+    const validResult = this.validateRole(validRole, userRole);
+
+    if (validResult) {
+      return true;
+    } else {
+      throw new ForbiddenException("アクセス権限がありません。");
+    }
+  }
+}
