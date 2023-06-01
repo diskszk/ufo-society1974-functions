1730c8b9877733a734ba1686acc3659fde0776c9
diskszk <ibalucky23@gmail.com>
2023-06-01 13:46:44 +0900

    fix: albumsConverter内でfirestore.Timestampを使わない

--- src/albums/albums.converter.ts
+++ src/albums/albums.converter.ts
@@ -1,16 +1,16 @@
 import { firestore } from "firebase-admin";
 import { DocumentData } from "firebase-admin/firestore";
 import { Album } from "ufo-society1974-definition-types";
+import { CreateAlbumDTO, UpdateAlbumDTO } from "./albums.dto";
 
 export const albumConverter = {
-  toFirestore(album: Album): DocumentData {
+  toFirestore(album: CreateAlbumDTO | UpdateAlbumDTO): DocumentData {
     return {
       description: album.description,
       imageFile: album.imageFile,
       publishedDate: album.publishedDate,
       title: album.title,
       published: false, // 新規作成時は非公開
-      createdAt: FirebaseFirestore.Timestamp.now(),
     };
   },
   fromFirestore(
@@ -25,7 +25,6 @@ export const albumConverter = {
       imageFile: data.imageFile,
       title: data.title,
       published: data.published,
-      createdAt: data.createdAt,
     };
   },
 };
