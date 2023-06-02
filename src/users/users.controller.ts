e41e6e8a17f33337be1c6bb809a96b291b1b7936
diskszk <ibalucky23@gmail.com>
2023-06-02 15:57:26 +0900

    feat: UsersControllerの仕様を追加する

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,18 +7,21 @@ import {
   NotFoundException,
   Param,
   Post,
+  Put,
   UseGuards,
 } from "@nestjs/common";
 import { User } from "./user.entity";
 import { role } from "../constants";
 import { AuthGuard } from "../auth/auth.guard";
-import { CreateUserDTO } from "./users.dto";
+import { CreateUserDTO, UpdateUserDTO } from "./users.dto";
 import { UsersService } from "./users.service";
 import { Role } from "../decorators/role.decorator";
 import { RoleGuard } from "../role/role.guard";
 import {
   ApiBadRequestResponse,
+  ApiBearerAuth,
   ApiCreatedResponse,
+  ApiNoContentResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiTags,
@@ -44,7 +47,7 @@ export class UsersController {
     description: "ユーザーIDと一致するユーザーを1件取得する",
   })
   @ApiNotFoundResponse({
-    description: "指定されたユーザーは存在しません。",
+    description: "IDと一致するユーザーは存在しません。",
   })
   async findUserById(@Param("userId") userId: string): Promise<User> {
     const targetUser = await this.usersService.findById(userId);
@@ -53,7 +56,7 @@ export class UsersController {
       throw new NotFoundException("指定されたユーザーは存在しません。");
     }
 
-    // 削除済みということなので404でよさそう
+    // 削除済みということなので404
     if (targetUser.isDeleted) {
       throw new NotFoundException("指定されたユーザーは存在しません。");
     }
@@ -80,10 +83,32 @@ export class UsersController {
     return this.usersService.create(user);
   }
 
+  @Put(":uid")
+  @Role(role.MASTER)
+  @UseGuards(RoleGuard)
+  @ApiBearerAuth()
+  @ApiNoContentResponse()
+  @ApiNoContentResponse({
+    description: "ユーザーの更新に成功する",
+  })
+  @ApiNotFoundResponse({
+    description: "IDと一致するユーザーは存在しません。",
+  })
+  async updateUser(@Body() user: UpdateUserDTO) {
+    return;
+  }
+
   // 論理削除だがクライアントからみたら削除なのでHTTP DELETEメソッドでよさそう
   @Delete(":userId")
   @Role(role.MASTER)
   @UseGuards(RoleGuard)
+  @ApiNoContentResponse()
+  @ApiNotFoundResponse({
+    description: "IDと一致するユーザーは存在しません。",
+  })
+  @ApiBadRequestResponse({
+    description: "IDと一致するユーザーは管理者のため削除できません。",
+  })
   async deleteUser(@Param("userId") userId: string) {
     const targetUser = await this.usersService.findById(userId);
 
